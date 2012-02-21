04c6148a17499050c78431a1d282863ff72531ac
mweber <>
2012-02-21 08:54:35 +0000

    Major modifications for computing fake rates

--- AdvancedROOTAnalyzer/root/fakerate.C
+++ AdvancedROOTAnalyzer/root/fakerate.C
@@ -1,3 +1,20 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TH3D.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "THashList.h"
+#include "TString.h"
+
+#include "plot.h"
+#include "stat.h"
+
+
 const char * get_bin_edges(const TH1 * histo, Int_t i, Int_t j, Int_t k)
 {
   static char buffer[255];
@@ -17,15 +34,15 @@ const char * get_bin_edges(const TH1 * histo, Int_t i, Int_t j)
   return buffer;
 }
 
-void tight_loose_ratio(const char * process = "qcd1520mu")
+TH1D * tight_loose_ratio(const char * process = "data_doublemu", bool save=true)
 {
   for (Int_t period = 0; period < gMaxPeriod; period++) {
     TFile f(Form("%s/%s.root", getpath(period), process));
     if (!f.IsOpen()) {
-      return;
+      return 0;
     }
-    TH3D * hTight3 = f.Get("h3_TightMuons");
-    TH3D * hLoose3 = f.Get("h3_LooseMuons");
+    TH3D * hTight3 = (TH3D *) f.Get("h3_TightMuons");
+    TH3D * hLoose3 = (TH3D *) f.Get("h3_LooseMuons");
     TH3D * hRatio3 = new TH3D(*hTight3);
     hRatio3->SetDirectory(0);
     hRatio3->SetName("hRatio3");
@@ -44,8 +61,8 @@ void tight_loose_ratio(const char * process = "qcd1520mu")
 
 	  if (nLoose < nTight || nLoose < 0) {
 	    cerr << "ERR: nLoose is wrong in your histogram" << endl;
-	    cout << "INFO: nLoose(" << get_bin_edges(hRatio, i, j, k) ") " << nLoose << endl;
-	    cout << "INFO: nTight(" << get_bin_edges(hRatio, i, j, k) ") " << nTight << endl;
+	    cout << "INFO: nLoose(" << get_bin_edges(hRatio3, i, j, k) << ") " << nLoose << endl;
+	    cout << "INFO: nTight(" << get_bin_edges(hRatio3, i, j, k) << ") " << nTight << endl;
 	  }
 	  if (nLoose != 0 && nRatio != nTight/nLoose) {
 	    cerr << "ERR: ROOT calculation went wrong" << endl;
@@ -62,8 +79,8 @@ void tight_loose_ratio(const char * process = "qcd1520mu")
     }
 
     // now do the same in 2D-Projection
-    hTight2 = (TH2D *) hTight3->Project3D("xy");
-    hLoose2 = (TH2D *) hLoose3->Project3D("xy");
+    TH2D * hTight2 = (TH2D *) hTight3->Project3D("yx");
+    TH2D * hLoose2 = (TH2D *) hLoose3->Project3D("yx");
     TH2D * hRatio2 = new TH2D(*hTight2);
     hRatio2->SetDirectory(0);
     hRatio2->SetName("hRatio2");
@@ -74,15 +91,15 @@ void tight_loose_ratio(const char * process = "qcd1520mu")
     // some sanity checks
     for (Int_t i = 1;  i < hRatio2->GetNbinsX()+1; i++) {
       for (Int_t j = 1; j < hRatio2->GetNbinsY()+1; j++) {
-	Double_t nLoose = hLoose2->GetBinContent(i, j, k);
-	Double_t nTight = hTight2->GetBinContent(i, j, k);
-	Double_t nRatio = hRatio2->GetBinContent(i, j, k);
-	Double_t error  = hRatio2->GetBinError(i, j, k);
+	Double_t nLoose = hLoose2->GetBinContent(i, j);
+	Double_t nTight = hTight2->GetBinContent(i, j);
+	Double_t nRatio = hRatio2->GetBinContent(i, j);
+	Double_t error  = hRatio2->GetBinError(i, j);
 
 	if (nLoose < nTight || nLoose < 0) {
 	  cerr << "ERR: nLoose is wrong in your histogram" << endl;
-	  cout << "INFO: nLoose(" << get_bin_edges(hRatio, i, j) ") " << nLoose << endl;
-	  cout << "INFO: nTight(" << get_bin_edges(hRatio, i, j) ") " << nTight << endl;
+	  cout << "INFO: nLoose(" << get_bin_edges(hRatio2, i, j) << ") " << nLoose << endl;
+	  cout << "INFO: nTight(" << get_bin_edges(hRatio2, i, j) << ") " << nTight << endl;
 	}
 	if (nLoose != 0 && nRatio != nTight/nLoose) {
 	  cerr << "ERR: ROOT calculation went wrong" << endl;
@@ -97,22 +114,145 @@ void tight_loose_ratio(const char * process = "qcd1520mu")
       }
     }
 
+    // now do the same in 1D-Projection
+    TH1D * hTight1 = (TH1D *) hTight3->Project3D("x");
+    TH1D * hLoose1 = (TH1D *) hLoose3->Project3D("x");
+    TH1D * hRatio1 = new TH1D(*hTight1);
+    hRatio1->SetDirectory(0);
+    hRatio1->SetName("hRatio1");
+    hRatio1->SetTitle("Tight/Loose ratio (T/L)");
+
+    // divide with binomial errors
+    hRatio1->Divide(hTight1, hLoose1, 1., 1., "B");
+
+    // make plots
     cout << "Period: " << gPeriod[period] << " Ratio OK." << endl;
     MakeCanvas();
     cd(1);
-    hRatio3->Draw();
+    hRatio1->Draw("le");
     cd(2);
-    hRatio2->Draw();
+    hRatio2->Draw("LEGO");
+    setopt(hRatio2);
+    hRatio2->GetXaxis()->SetTitle("#mu p_{T} [GeV]");
+    hRatio2->GetYaxis()->SetTitle("#mu #eta");
+    hRatio2->GetZaxis()->SetTitle("T/L ratio");
 
-    const char * fakeRateFileName = "FakeRate.root";
-    TFile * fakeRateFile = new TFile(fakeRateFileName, "RECREATE");
-    if (fakeRateFile == 0 || !fakeRateFile->IsOpen()) {
-      cerr << "ERR: Could not open fake rate file " << fakeRateFileName << endl;
+    if (save) {
+      const char * fakeRateFileName = "FakeRate.root";
+      TFile * fakeRateFile = new TFile(fakeRateFileName, "RECREATE");
+      if (fakeRateFile == 0 || !fakeRateFile->IsOpen()) {
+	cerr << "ERR: Could not open fake rate file " << fakeRateFileName << endl;
+	return 0;
+      }
+      hRatio2->Write();
+      hRatio3->Write();
+      fakeRateFile->Close();
+      delete fakeRateFile;
+    }
+  }
+  // return one-dimensional plot
+  return 0;
+}
+
+void tightlooseplots(const char * sele = "tightloose")
+{
+  selection("sele");
+  cd(1);
+  plot("nTL_met");
+  logy();
+  cd(2);
+  plot("nTL_HT");
+  print("tightloose_1.pdf");  
+}
+
+void doublefakeplots(const char * sele = "doublefake")
+{
+  selection(sele); 
+  cd(1);
+  plot("DF_eta"); 
+  cd(2); 
+  plot("DF_pt");
+  print("doublefake_1.pdf");
+
+  
+  cd(1);
+  plot("cutflow");
+  liny();
+  max(70);
+  cd(2);
+  plot("m_mumu");
+  rebin(5);
+  stat();
+  print("doublefake_2.pdf");
+
+  cd(1); 
+  plot("fFakeRate", "1.");
+  cd(2);
+  TF1 * f1 = new TF1("f1", "x/(1.-x)", 0, 0.5);
+  f1->Draw();
+  print("doublefake_3.pdf");
+}
+
+double get_doublefakes()
+{
+  MakeCanvas();
+  TFile f("/user/mweber/doublefake/2011/data_doublemu.root");
+  TH1D * hmu = (TH1D *) f.Get("h1_m_mumu");
+  hmu->SetDirectory(0);
+  setopt(hmu);
+  hmu->Rebin(5);
+  hmu->Draw();
+  double N = hmu->Integral();
+  cout << "Found " << N << " events in final selection" << endl;
+  cd(2);
+  TH1D * hcut = (TH1D *) f.Get("h1_cutflow");
+  hcut->SetDirectory(0);
+  setopt(hcut);
+  hcut->SetMarkerStyle(8);
+  hcut->SetMaximum(70.);
+  hcut->Draw("ep");
+  N = hcut->GetBinContent(12.);
+  cout << "Got " << N << " events from cut flow" << endl;
+  return N;
+}
+
+const char * stages[] = { 
+"weight","pileup rew.","redo skim","trigger","objectID","cleaning","tightloose","jetid","doublefake","singlefake","standard","muonID","met","m_mumu","charge"
+};
+
+void check_all_files()
+{
+  for (int i = 0; i < 167; i++) {
+    const char * fname = Form("/user/mweber/doublefake2/2011/data_doublemu_%d.root", i);
+    TFile * f = new TFile(fname);
+    if (f == 0 || !f->IsOpen()) {
+      cout << "Problem opening file " << fname << endl;
       return;
     }
-    hRatio2->Write();
-    hRatio3->Write();
-    fakeRateFile->Close();
-    delete fakeRateFile;
+    cout << fname << endl;
+    TH1D * h1 = (TH1D *) f->Get("cutflow");
+    if (h1 == 0) {
+      cout << "Error getting histogram from file" << endl;
+      return;
+    }
+    if (h1->GetNbinsX() != 15) {
+      cout << "histogram has " << h1->GetNbinsX() << " bins " << endl;
+    }
+    // iterate over bin names
+    THashList * hl = h1->GetXaxis()->GetLabels();
+    int j = 0;
+    TIter next(hl);
+    TObjString * s;
+    while ((s = (TObjString *) next())) {
+      if (strcmp(s->GetString().Data(), stages[j])) {
+	cout << "string mismatch: expected: " << stages[j] 
+	     << " but got instead: " << s->GetString().Data() << endl;
+      }
+      j++;
+    }
+    if (h1 ->GetBinContent(7) != 0) {
+      cout << "bin 7 contains " << h1->GetBinContent(7) << "events!" << endl;
+    }
+    delete f;
   }
 }
