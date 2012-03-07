b2d32bf223cfcbb4b8c5393fca70894c72e8277c
mweber <>
2012-03-07 08:15:06 +0000

    bugfix: first apply CMS TDR style, then overwrite with own style

--- AdvancedROOTAnalyzer/root/rootlogon.C
+++ AdvancedROOTAnalyzer/root/rootlogon.C
@@ -5,6 +5,10 @@
   // gROOT->SetStyle("Plain");
   gStyle->SetPalette(1);
 
+  // load and apply CMS TDR style for plots
+  gROOT->LoadMacro(Form("%s/root/tdrstyle.C", getenv("ARASYS")));
+  setTDRStyle();
+
   // load main macros
   gROOT->LoadMacro(Form("%s/root/plot.C+", getenv("ARASYS")));
   gROOT->LoadMacro(Form("%s/root/stat.C+", getenv("ARASYS")));
@@ -14,10 +18,6 @@
 
   // if you want to debug what is happening, uncomment next line
   // gLogLevel = 100;
-
-  // load and apply CMS TDR style for plots
-  gROOT->LoadMacro(Form("%s/root/tdrstyle.C", getenv("ARASYS")));
-  setTDRStyle();
 
   printf("rootlogon.C loaded successfully.\n");
 }
