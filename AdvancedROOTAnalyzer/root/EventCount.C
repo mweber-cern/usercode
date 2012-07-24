828e9c2f48f663c31b7ce1b9d6194e77c383472e
mweber <>
2012-07-24 13:18:54 +0000

    Added support for reading histogram counters, now more verbose output

--- AdvancedROOTAnalyzer/root/EventCount.C
+++ AdvancedROOTAnalyzer/root/EventCount.C
@@ -1,15 +1,51 @@
 #include <stdlib.h>
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 
 #include "TROOT.h"
+#include "TObject.h"
+#include "TFile.h"
 #include "TChain.h"
 #include "TSystem.h"
+#include "TH1F.h"
 
 int gLogLevel = 3;
 
 using namespace std;
+
+TObject * get_object(const char * filename, const char * objectname)
+{
+  // try to find out if file is already opened
+  TFile * f = (TFile *) gROOT->GetListOfFiles()->FindObject(filename);
+  if (f != 0) {
+//     cout << "File " << filename << " was already open" << endl;
+  }
+  else {
+    f = new TFile(filename, "READ");
+    if (f == 0) {
+      cerr << "ERROR: Could not create TFile object for " << filename << endl;
+      return 0;
+    }
+    if (!f->IsOpen()) {
+      cerr << "ERROR: Could not open file " << filename << endl;
+      return 0;
+    }
+  }
+  TObject * obj = f->Get(objectname);
+  if (obj == 0) {
+    cerr << "Could not get object " << objectname << " from file " << filename << endl;
+    return 0;
+  }
+  // put histograms in main directory
+  if (obj->IsA()->InheritsFrom("TH1")) {
+    TH1 * h = (TH1 *) obj;
+    h->SetDirectory(0);
+  }
+  delete f;
+  return obj;
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -47,16 +83,29 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
+  long int counters[3] = { 0 };
+
   // Add all root files to a TChain
   if (gLogLevel)
     cout << "Adding files to TChain" << endl;
-
+  
   TChain * chain = new TChain("ACSkimAnalysis/allData");
   TIter next(filelist);
   while (TObjString * obj = (TObjString *) next()) {
     chain->Add(obj->GetString());
     if (gLogLevel)
       cout << "FILE: " << obj->GetString() << " added to TChain" << endl;
+
+    // open file, get histograms, add to counters
+    TH1F * h = (TH1F *) get_object(obj->GetString(), "ACSkimAnalysis/h_counters");
+    if (h == 0) {
+      cerr << "ERR: Could not find histogram h_counters in file" << endl;
+    }
+    else {
+      counters[0] += h->GetBinContent(1);
+      counters[1] += h->GetBinContent(2);
+      counters[2] += h->GetBinContent(3);
+    }
   }
 
   // Delete all filelist elements and the list itself
@@ -65,28 +114,58 @@ int main(int argc, char *argv[])
   delete filelist;
   delete basepath;
 
-  long int events_from_tree = chain->GetEntries();
-  cout << "Found a total of " << events_from_tree << " events in chain" << endl;
 
+  long int events_from_tree = chain->GetEntries();
+
+  long int fcounter[3] = { 0 };
   // open text file with event count
   char buffer[255];
   ifstream infile(argv[2]);
   while (infile.getline(buffer, 255)) {
     long int nev;
-    if (strstr(buffer, "written"))
-      cout << buffer << endl;
+    if (sscanf(buffer, " ==>  #events read    :  %ld", &nev ) == 1) {
+      fcounter[0] = nev;
+    }
+    if (sscanf(buffer, " ==>  #events presel  :  %ld", &nev ) == 1) {
+      fcounter[1] = nev;
+    }
     if (sscanf(buffer, " ==>  #events written :  %ld", &nev ) == 1) {
-      cout << "Events from file " << argv[2] << " : " << nev << endl;
-      if (nev != events_from_tree) {
-	cerr << endl << "ERROR: Number of events from tree and file do not match!" << endl;
-	exit(2);
-      }
-      else {
-	cout << endl << "OK: Events match!!" << endl;
-	break;
-      }
+      fcounter[2] = nev;
     }
   }
+
+  // Output a table
+  cout << "Source                      Read         Presel        Written" << endl;
+  cout << "counter.txt      "
+       << setw(15) << fcounter[0]
+       << setw(15) << fcounter[1]
+       << setw(15) << fcounter[2] << endl;
+  cout << "h_counter        " 
+       << setw(15) << counters[0]
+       << setw(15) << counters[1]
+       << setw(15) << counters[2] << endl;
+  cout << "Tree             " 
+       << setw(45) << events_from_tree << endl;
+
+  bool OK = true;
+  // Analyse numbers from different sources
+  if (fcounter[0] != counters[0]) {
+    cerr << "ERROR: Read events do not agree" << endl;
+    OK = false;
+  }
+  if (fcounter[1] != counters[1]) {
+    cerr << "ERROR: Presel events do not agree" << endl;
+    OK = false;
+  }
+  if (fcounter[2] != counters[2] || fcounter[2] != events_from_tree) {
+    cerr << "ERROR: Written events do not agree" << endl;
+    OK = false;
+  }
+  if (OK) {
+    cout << endl << "OK: Events match!!" << endl;
+  }
+  else 
+    exit(2);
 
   return 0;
 }
