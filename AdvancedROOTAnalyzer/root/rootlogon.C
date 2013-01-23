d5244856d844eb909cfbdcc58d2111040011b10c
radziej <>
2013-01-23 13:08:33 +0000

    Nice color palette added

--- AdvancedROOTAnalyzer/root/rootlogon.C
+++ AdvancedROOTAnalyzer/root/rootlogon.C
@@ -1,9 +1,23 @@
-{
+void setNiceColorPalette () {
+    const Int_t NRGBs = 5;
+    const Int_t NCont = 255;
+
+    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+    Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+    Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+    TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+    gStyle->SetNumberContours(NCont);
+    
+    printf("Using nice color palette \n");
+}
+
+void rootlogon () {
   printf("Loading rootlogon.C...\n");
 
   // corrections to ugly ROOT standard style
   // gROOT->SetStyle("Plain");
-  gStyle->SetPalette(1);
+  // gStyle->SetPalette(1);
 
   // load and apply CMS TDR style for plots
   gROOT->LoadMacro(Form("%s/root/tdrstyle.C", getenv("ARASYS")));
@@ -17,8 +31,10 @@
   gROOT->LoadMacro(Form("%s/root/rpv.C+", getenv("ARASYS")));
   gROOT->LoadMacro(Form("%s/root/systematics.C+", getenv("ARASYS")));
 
+  setNiceColorPalette();
+  
   // set up everything
-  setup("../config/plot.cfg");
+  setup("plot.cfg");
 
   // if you want to debug what is happening, uncomment next line
   // gLogLevel = 100;
