5886c4ea473580c86292698753f0d0535cc5c8ef
mweber <>
2013-02-15 11:02:49 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-08-00'.
    Sprout from analysis_2011 2013-02-15 11:02:48 UTC mweber 'Included updates from HEAD'
    Cherrypick from master 2013-02-13 14:42:06 UTC mweber 'condor_top now tells people to press Ctrl-C':
        AdvancedROOTAnalyzer/bin/ara.py
        AdvancedROOTAnalyzer/bin/condor_top
        AdvancedROOTAnalyzer/bin/submit.py
        AdvancedROOTAnalyzer/bin/systematics.py
        AdvancedROOTAnalyzer/bin/tightloose.py
    Delete:
        AdvancedROOTAnalyzer/bin/tightloose_syst.sh

---
+++ AdvancedROOTAnalyzer/bin/systematics.py
@@ -0,0 +1,93 @@
+#!/bin/env python
+
+import os,sys,optparse
+import ara
+
+# define systematics jobs
+
+mydict = eval("""
+{ 
+  "LooseMuonRelIso" : 
+  { 
+    "default" : "0.4", 
+    "syst" : ( "0.3", "0.5", "0.6", "0.8" )
+  },
+  "TL_jetpt_min" :
+  {
+    "default" : "40",
+    "syst" : ( "50", "60", "80")
+  },
+  "FakeRateMethod" :
+  { 
+    "default" : "lastbin",
+    "syst"  : ( "zero" )
+  },
+  "TL_njets_min" :
+  { 
+    "default" : "2.",
+    "syst" : ( "1." )
+  }
+}
+""")
+
+def create_and_submit_jobs(template, replacements, syst, value, basename):
+    """Create configuration file "destination" by replacing the words 
+    in the dictionary, submit and collect jobs"""
+    suffix = "%s_%s_%s" % ( basename, syst, value)
+    destination = os.environ["ARASYS"]+"/config/analyzer_%s.cfg"  % ( suffix )
+    ara.create_config_file(template, replacements, destination)
+    print "Starting systematics", suffix
+    os.system("tightloose.py -b " + suffix)
+
+######################################################################
+# main
+def main():
+    usage = """
+
+%prog configfile
+
+Submit systematic studies jobs for the given configuration file. Creates necessary
+configuration files and submits and collects the jobs."""
+    optParser = optparse.OptionParser(usage)
+    optParser.add_option("-c", "--config", dest="cfgfile",
+                         help="global ARA configuration file",
+                         default=ara.defaultConfigFileName)
+
+    global options
+    (options, args) = optParser.parse_args()
+    if len(args) < 1 or len(args) > 1:
+        optParser.print_help()
+        return 1
+
+    basename = args[0]
+    basepath = os.environ["ARASYS"]+"/config/analyzer_"+basename
+    templateFileName = basepath+".cfg"
+
+    # Check if template file exists
+    if not os.path.isfile(templateFileName):
+        print "File "+templateFileName+" not found. Exiting."
+        return 2
+
+    # Loop over all systematics
+    for syst in mydict:
+        print "Starting systematics", syst, "replacing", mydict[syst]["default"], "with", mydict[syst]["syst"]
+
+        repMap = { }
+        # Switch off writing tree contents to file, not necessary for systematics,
+        # just taking too much disk space
+        repMap["FillTree: true"] = "FillTree: false"
+        search = syst+': '+mydict[syst]["default"]
+        if isinstance(mydict[syst]["syst"], tuple):
+            for value in mydict[syst]["syst"]:
+                repMap[search] = syst+': '+value
+                create_and_submit_jobs(templateFileName, repMap, syst, value, basename)
+        else:
+            value = mydict[syst]["syst"]
+            repMap[search] = syst+': '+value
+            create_and_submit_jobs(templateFileName, repMap, syst, value, basename)
+        
+######################################################################
+# entry point if executed
+if __name__ == "__main__":
+    sys.exit(main())
+
