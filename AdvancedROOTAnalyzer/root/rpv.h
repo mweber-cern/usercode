51b5af25ad3cd4e9a664e7ea03fc6f11ad8e4475
mweber <>
2013-02-15 10:48:50 +0000

    This commit was manufactured by cvs2svn to create tag 'default36'.
    Sprout from analysis_2011 2013-02-15 10:48:49 UTC mweber 'Update some 2D histogram binning to be more consistent'
    Delete:
        AdvancedROOTAnalyzer/README
        AdvancedROOTAnalyzer/TODO
        AdvancedROOTAnalyzer/bin/.cvsignore
        AdvancedROOTAnalyzer/bin/addblock.sh
        AdvancedROOTAnalyzer/bin/ara.py
        AdvancedROOTAnalyzer/bin/checklog.py
        AdvancedROOTAnalyzer/bin/collect.py
        AdvancedROOTAnalyzer/bin/condor_top
        AdvancedROOTAnalyzer/bin/dcache_transfer.py
        AdvancedROOTAnalyzer/bin/gencutflow.py
        AdvancedROOTAnalyzer/bin/job_starter.sh
        AdvancedROOTAnalyzer/bin/merge.py
        AdvancedROOTAnalyzer/bin/mysrmls.py
        AdvancedROOTAnalyzer/bin/mysrmrm.py
        AdvancedROOTAnalyzer/bin/pickEvents.sh
        AdvancedROOTAnalyzer/bin/setup.sh
        AdvancedROOTAnalyzer/bin/submit.py
        AdvancedROOTAnalyzer/bin/tightloose_syst.sh
        AdvancedROOTAnalyzer/config/.cvsignore
        AdvancedROOTAnalyzer/config/Fall11_MC_pileup_truth.root
        AdvancedROOTAnalyzer/config/Weight3D.root
        AdvancedROOTAnalyzer/config/analyzer_default.cfg
        AdvancedROOTAnalyzer/config/analyzer_doublefake.cfg
        AdvancedROOTAnalyzer/config/analyzer_singlefake.cfg
        AdvancedROOTAnalyzer/config/ara.cfg
        AdvancedROOTAnalyzer/config/condor_template.cfg
        AdvancedROOTAnalyzer/config/histAllData.root
        AdvancedROOTAnalyzer/config/plot.cfg
        AdvancedROOTAnalyzer/config/plot_mu8_jet40.cfg
        AdvancedROOTAnalyzer/config/plot_singlemu.cfg
        AdvancedROOTAnalyzer/config/plot_tightloose.cfg
        AdvancedROOTAnalyzer/config/xs_2011.cfg
        AdvancedROOTAnalyzer/config/xs_2011_mu8_jet40.cfg
        AdvancedROOTAnalyzer/config/xs_2011_singlemu.cfg
        AdvancedROOTAnalyzer/config/xs_2012.cfg
        AdvancedROOTAnalyzer/root/.cvsignore
        AdvancedROOTAnalyzer/root/EventCount.C
        AdvancedROOTAnalyzer/root/fakerate.C
        AdvancedROOTAnalyzer/root/fakerate.h
        AdvancedROOTAnalyzer/root/fit.C
        AdvancedROOTAnalyzer/root/plot.C
        AdvancedROOTAnalyzer/root/plot.h
        AdvancedROOTAnalyzer/root/quickdraw.C
        AdvancedROOTAnalyzer/root/rootlogon.C
        AdvancedROOTAnalyzer/root/rpv.C
        AdvancedROOTAnalyzer/root/rpv.h
        AdvancedROOTAnalyzer/root/scan_7TeV_ud_smuon.txt
        AdvancedROOTAnalyzer/root/stat.C
        AdvancedROOTAnalyzer/root/stat.h
        AdvancedROOTAnalyzer/root/systematics.C
        AdvancedROOTAnalyzer/root/tdrstyle.C
        AdvancedROOTAnalyzer/root/xsection.C
        AdvancedROOTAnalyzer/root/xsection.h

--- AdvancedROOTAnalyzer/root/rpv.h
+++
@@ -1,8 +0,0 @@
-#ifndef __rpv_h_
-#define __rpv_h_
-
-#include "TH2D.h"
-
-void get2dstatistics(const TH2D * h2, double values[6][2]);
-
-#endif
--- AdvancedROOTAnalyzer/root/stat.h
+++
@@ -1,6 +0,0 @@
-#ifndef _stat_h_
-#define _stat_h_
-
-void stat(Double_t low = 0, Double_t up = 0);
-
-#endif
